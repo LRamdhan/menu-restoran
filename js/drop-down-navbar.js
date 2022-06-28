7b8c07f1d84a07064bc69fc0a46f963fae434349
ubed <ijul@gmail.com>
2022-06-28 10:38:43 +0700

    penambahan navbar
    masih belum selesai

---
+++ js/drop-down-navbar.js
@@ -0,0 +1,13 @@
+const hamberger = document.querySelector('nav .hamberger');
+const dropDown = hamberger.nextElementSibling;
+
+hamberger.addEventListener('click', event => {
+    const child = hamberger.children;
+    child[0].classList.toggle('miring-kiri');
+    child[1].classList.toggle('mengecil');
+    child[2].classList.toggle('miring-kanan');
+    dropDown.classList.toggle('drop-down-normal');
+});
+
+
+
